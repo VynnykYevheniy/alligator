b118baa646575fb1aa3770bfc1ad06661f23b7ad
Yevheniy Vynnyk <vinnik27734@gmail.com>
2024-02-11 16:38:56 +0200

    alligator-3: create core

---
+++ src/test/java/com/alligator/AlligatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alligator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlligatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
